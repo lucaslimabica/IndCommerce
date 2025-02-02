700f1b7be449056507966765855ddf0b960aa5ef
Lucas de Lima Bica <delimabica@gmail.com>
2025-02-02 14:51:57 +0000

    Creating Pages and Routes to Them

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
 // src/App.js
 import React from 'react';
-import Header from './elements/Header';
-import Greeting from './props/Greetings';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
+// Importe o CSS do Bootstrap (se ainda não importou no index.js)
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importe suas páginas
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <main>
-        <section>
-          <Greeting name="Lucas" />
-          <p>I've been write on the App.js file.</p>
-        </section>
-        <section>
-          <ul>
-            <li><Greeting name="Maria" /></li>
-            <li><Greeting name="Helena" /></li>
-            <li>João</li>
-          </ul>
-        </section>
-      </main>
-      <footer>
-        <p>Foooooter</p>
-      </footer>
-    </div>
+    <Router>
+      {/* Navbar com elementos do Bootstrap */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">Meu Site</Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Alterna navegação"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Início</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">Sobre</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* Configuração das rotas */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
